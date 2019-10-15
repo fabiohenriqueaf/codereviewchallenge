0283bfad1da76dca5d421ed222ba21a8a1abe991
Francisco Soares <fmssn@cin.ufpe.br>
2019-10-15 16:44:46 -0300

    Initial project files

---
+++ TemperatureConverter/TemperatureConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemperatureConverter
+//
+//  Created by Francisco Soares Neto on 15/10/19.
+//  Copyright © 2019 Francisco Soares Neto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
